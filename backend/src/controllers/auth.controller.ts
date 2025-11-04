20a7f39ed2188851a5982b68fbfe6a38cfd7bd65
Xnaizer <seykig028@gmail.com>
2025-11-04 14:38:56 +0700

    feat: auth init

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,93 @@
+import type { Request, Response } from "express";
+import * as Yup from 'yup'
+
+type TRegister = {
+    fullName: string;
+    username: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
+// export default {
+//     register(req: Request,res: Response) {
+//         const {
+//             fullName,
+//             username,
+//             email,
+//             password,
+//             confirmPassword
+//         } = req.body as unknown as TRegister; 
+//     }
+// }
+
+    const registerValidateSchema = Yup.object({
+        fullName: Yup.string().required(),
+        username: Yup.string().required(),
+        email: Yup.string().required(),
+        password: Yup.string().required(),
+        confirmPassword: Yup.string().required().oneOf([Yup.ref('password'), ""], "Password not match")
+    })
+
+export default {
+    async register(req: Request<{},{},TRegister>,res: Response) {
+        const {
+            fullName,
+            username,
+            email,
+            password,
+            confirmPassword
+        } = req.body; 
+
+        try{
+            await registerValidateSchema.validate({
+                fullName,
+                username,
+                email,
+                password,
+                confirmPassword
+            });
+
+            res.status(200).json({
+                message: "Succes Registrations!",
+                data: {
+                    fullName,
+                    username,
+                    email,
+                }
+            });
+
+        } catch (error){
+            const err = error as unknown as Error;
+            
+            res.status(400).json({
+                message: err.message,
+                data: null
+            })
+        }
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+// Request<{},{},TRegister
+
+// contoh : 
+
+// interface Params { id: string; }
+// interface Body { email: string; password: string; }
+// interface Query { lang?: string; }
+
+// function updateUser(req: Request<Params, {}, Body, Query>, res: Response) {
+//   console.log(req.params.id);   // dari /users/:id
+//   console.log(req.body.email);  // dari form JSON
+//   console.log(req.query.lang);  // dari ?lang=en
+// }
--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 import dummyController from '../controllers/dummy.controller'
+import authController from '../controllers/auth.controller'
 
 const router = express.Router();
+
+
+
+router.post("/auth/register", authController.register);
+
 
 
 router.get('/dummy', dummyController.dummy);
