dedae7b2e81de820ce1b74dacc27a49f3b69c4f4
Xnaizer <seykig028@gmail.com>
2025-11-04 13:34:11 +0700

    adding gitignore

--- backend/src/controllers/dummy.controller.ts
+++ backend/src/controllers/dummy.controller.ts
@@ -3,7 +3,7 @@ import type {Request, Response} from 'express';
 export default {
     dummy(req:  Request, res: Response) {
         res.status(200).json({
-            message: 'Success hit endpoint /dummy !',
+            message: 'Success hit endpoint /dummy ! - hasil berubah',
             data: "OK"
         })
     },
