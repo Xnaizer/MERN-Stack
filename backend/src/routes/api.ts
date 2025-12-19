a34c66c26b75767a3e0de758f609db5a6a1153ce
Xnaizer <seykig028@gmail.com>
2025-12-19 20:23:11 +0700

    feat: implement auth local api

--- backend/src/routes/api.ts
+++ backend/src/routes/api.ts
@@ -1,8 +1,18 @@
 import express from 'express';
+
+
+// import authentications
+import authController from '../controllers/auth.controller';
+
 import dummyController from '../controllers/dummy.controller';
 
 const router = express.Router();
 
+// authentications controller
+router.post('/auth/register', authController.register);
+
+
+// dummy controller
 router.get('/dummy', dummyController.dummy);
 
 export default router;
