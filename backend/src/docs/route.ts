09c93fb1c37c8ae024d9c7fbabc91f0cf9ad9603
Xnaizer <seykig028@gmail.com>
2025-12-25 11:49:43 +0700

    feat: trying fix swagger for dev

--- backend/src/docs/route.ts
+++ backend/src/docs/route.ts
@@ -2,15 +2,24 @@ import type { Express, Request, Response } from "express";
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from "./swagger";
 
+import fs from "fs";
+import path from "path";
+
 export default function docs(app: Express) {
 
-    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
-        customCssUrl: "https://cdn.jsdelivr.net/npm/swagger-ui-dist/swagger-ui.css"
-    }));
+    const css = fs.readFileSync(path.resolve(__dirname, "../../node_modules/swagger-ui-dist/swagger-ui.css"), "utf-8")
+
+    app.use(
+        "/api-docs",
+        swaggerUi.serve,
+        swaggerUi.setup(swaggerSpec, {
+            customCss: css
+        })
+    );
 
     app.get("/api-docs.json", (req: Request, res: Response) => {
         res.setHeader("Content-Type", "application/json");
         res.send(swaggerSpec);
-    })
+    });
 
 }
