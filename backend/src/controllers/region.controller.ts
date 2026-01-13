aa2dc7e293afafa8bf45149f0057b4744d6eec2c
Xnaizer <seykig028@gmail.com>
2026-01-13 22:17:12 +0700

    fix: response handling

--- backend/src/controllers/region.controller.ts
+++ backend/src/controllers/region.controller.ts
@@ -3,98 +3,96 @@ import response from '../utils/response';
 import RegionModel from '../models/region.model';
 
 export default {
-  async findByCity(res: Response, req: Request) {
+  async findByCity(req: Request, res: Response) {
     try {
-        const { name } = req.query;
-        const result = await RegionModel.findByCity(`${name}`);
+      const { name } = req.query;
+      const result = await RegionModel.findByCity(`${name}`);
 
-        if(!result) {
-            return response.notFound(res, 'City not found!');
-        }
+      if (!result || result.length === 0) {
+        return response.notFound(res, 'City not found!');
+      }
 
-        response.success(res, result, 'Success find a city')
-
+      return response.success(res, result, 'Success find a city');
     } catch (error) {
-        response.error(res, error, "Failed finding a city" )
+      return response.error(res, error, 'Failed finding a city');
     }
   },
 
-  async getAllProvinces(res: Response, req: Request) {
+  async getAllProvinces(req: Request, res: Response) {
     try {
-        const result = await RegionModel.getAllProvinces();
+      const result = await RegionModel.getAllProvinces();
 
-        if(!result) {
-            return response.notFound(res,'Failed get all provinces')
-        }
+      if (!result || result.length === 0) {
+        return response.notFound(res, 'Failed get all provinces');
+      }
 
-        response.success(res, result, 'Success to get all provinces')
-
+      return response.success(res, result, 'Success to get all provinces');
     } catch (error) {
-        response.error(res, error, 'Failed get all provinces')
+      return response.error(res, error, 'Failed get all provinces');
     }
   },
 
-  async getProvince(res: Response, req: Request) {
+  async getProvince(req: Request, res: Response) {
     try {
-        const { id } = req.params;
+      const { id } = req.params;
 
-        const result = await RegionModel.getProvince(Number(id));
+      const result = await RegionModel.getProvince(Number(id));
 
-        if(!result) {
-            return response.notFound(res, 'Province not found');
-        }
+      if (!result || result.length === 0) {
+        return response.notFound(res, 'Province not found');
+      }
 
-        response.success(res, result, 'Success get a province');
-
+      return response.success(res, result, 'Success get a province');
     } catch (error) {
-        response.error(res, error, 'Failed get a province');
+      return response.error(res, error, 'Failed get a province');
     }
   },
 
-  async getRegency(res: Response, req: Request) {
+  async getRegency(req: Request, res: Response) {
     try {
-        const { id } = req.params;
+      const { id } = req.params;
 
-        const result = await RegionModel.getRegency(Number(id));
+      const result = await RegionModel.getRegency(Number(id));
 
-        if(result) {
-            return response.notFound(res, 'Regency not found');
-        }
+      if (!result || result.length === 0) {
+        return response.notFound(res, 'Regency not found');
+      }
+
+      return response.success(res, result, 'Success get a regency');
     } catch (error) {
-        response.error(res, error, 'Failed get a regency');
+      return response.error(res, error, 'Failed get a regency');
     }
   },
 
-  async getDistrict(res: Response, req: Request) {
+  async getDistrict(req: Request, res: Response) {
     try {
-        const { id } = req.params;
-        
-        const result = await RegionModel.getDistrict(Number(id));
+      const { id } = req.params;
 
-        if(!result)  {
-            return response.notFound(res, 'District not found');
-        }
+      const result = await RegionModel.getDistrict(Number(id));
 
-        response.success(res, result, 'Success get a district');
+      if (!result || result.length === 0) {
+        return response.notFound(res, 'District not found');
+      }
 
+      return response.success(res, result, 'Success get a district');
     } catch (error) {
-        response.error(res, error, 'Failed get a district');
+      return response.error(res, error, 'Failed get a district');
     }
   },
 
-  async getVillage(res: Response, req: Request) {
+  async getVillage(req: Request, res: Response) {
     try {
-        const { id } = req.params;
+      const { id } = req.params;
 
-        const result = await RegionModel.getVillage(Number(id));
+      const result = await RegionModel.getVillage(Number(id));
 
-        if(!result) {
-            return response.notFound(res, 'Village not found')
-        }
+      if (!result || result.length === 0) {
+        return response.notFound(res, 'Village not found');
+      }
 
-        response.success(res, result, 'Success get a village');
+      return response.success(res, result, 'Success get a village');
     } catch (error) {
-        response.error(res, error, 'Failed get a village');
+      return response.error(res, error, 'Failed get a village');
     }
   },
 };
