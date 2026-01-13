7f735004af4567761e356ba643de1106b1cd8a4f
Xnaizer <seykig028@gmail.com>
2026-01-13 21:37:34 +0700

    feat: region route added

--- backend/src/routes/region.route.ts
+++ backend/src/routes/region.route.ts
@@ -1,12 +1,13 @@
 import express from 'express';
+import regionController from '../controllers/region.controller';
 
 const regionRoute = express.Router();
 
-regionRoute.get('/regions');
-regionRoute.get('/regions/:id/province');
-regionRoute.get('/regions/:id/regency');
-regionRoute.get('/regions/:id/district');
-regionRoute.get('/regions/:id/village');
-regionRoute.get('/regions-search');
+regionRoute.get('/regions', regionController.getAllProvinces);
+regionRoute.get('/regions/:id/province', regionController.getProvince);
+regionRoute.get('/regions/:id/regency', regionController.getRegency);
+regionRoute.get('/regions/:id/district', regionController.getDistrict);
+regionRoute.get('/regions/:id/village', regionController.getVillage);
+regionRoute.get('/regions-search', regionController.findByCity);
 
 export default regionRoute;
