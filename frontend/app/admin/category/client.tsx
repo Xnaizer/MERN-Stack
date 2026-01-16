0496ebc1d4f11458cd314344b594b3aeef5b4d3e
Xnaizer <seykig028@gmail.com>
2026-01-16 21:42:44 +0700

    feat: implement category mockup design nextui

--- frontend/app/admin/category/client.tsx
+++ frontend/app/admin/category/client.tsx
@@ -1,9 +1,86 @@
+'use client'
+import DataTable from "@/components/ui/DataTable/DataTable";
+import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@heroui/react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Key, ReactNode, useCallback } from "react";
+import { CiMenuKebab } from "react-icons/ci";
+import { COLUMN_LISTS_CATEGORY } from "./category.constant";
 
 const CategoryClient: React.FC = () => {
+
+    const { push } = useRouter();
+
+    const renderCell = useCallback((
+        category: Record<string, unknown>,
+        columnKey: Key
+    ) => {
+        const cellValue = category[columnKey as keyof typeof category];
+
+        switch(columnKey) {
+            case "icon":
+                return (
+                    <Image 
+                        src={`${cellValue}`} 
+                        alt="icon"
+                        width={100}
+                        height={200}
+                    />
+                )
+            case "actions":
+                return (
+                    <Dropdown>
+                        <DropdownTrigger>
+                            <Button 
+                                isIconOnly
+                                size="sm"
+                                variant="light"
+                            >
+                                <CiMenuKebab 
+                                    className="text-default-700"
+                                />
+                            </Button>
+                        </DropdownTrigger>
+                        <DropdownMenu>
+                            <DropdownItem
+                                key="detail-category-button" 
+                            onPress={() => push(`/admin/category/${category._id}`)}
+                            >
+                                Detail Category
+                            </DropdownItem>
+
+                            <DropdownItem
+                                key="delete-category" 
+                                className="text-danger-500"
+                            >  
+                                Delete
+                            </DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                )
+            default: 
+                return cellValue as ReactNode;
+        }
+
+    },[push])
+
     return (
-        <div>
-            <p>category client</p>
-        </div>
+        <section>
+            <DataTable
+                renderCell={renderCell}
+                colums={COLUMN_LISTS_CATEGORY}
+                data={[
+                    {
+                        _id: '123',
+                        name: "category 1",
+                        description: "description 1",
+                        icon: "/images/general/logo.png"
+                    }
+                ]}
+            >
+
+            </DataTable>
+        </section>
     )
 }
 
--- frontend/app/admin/category/page.tsx
+++ frontend/app/admin/category/page.tsx
@@ -4,12 +4,14 @@ import CategoryClient from "./client";
 
 export const metadata: Metadata = {
   title: "Category",
-  description: "Category Admin",
+  description: "List of all Categories, create new category, and manage existing categories",
 };
 
 export default function AdminPage() {
   return (
     <DashboardLayout type="admin">
+      <h1 className="text-slate-800 font-bold text-4xl mt-8">{metadata.title as string}</h1>
+      <h2 className="text-slate-700 text-xl pt-3 mb-8">{metadata.description as string}</h2>
       <CategoryClient />
     </DashboardLayout>
   );
