6a79827067e7b2868a9f193ae1224e317e2dc7d0
Xnaizer <seykig028@gmail.com>
2026-01-18 23:32:58 +0700

    feat(category page): adding icon for design

--- frontend/app/admin/category/page.tsx
+++ frontend/app/admin/category/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import DashboardLayout from "@/components/dashboard/layout";
 import CategoryClient from "./client";
+import { CgLayoutList } from "react-icons/cg";
 
 export const metadata: Metadata = {
   title: "Category",
@@ -10,8 +11,13 @@ export const metadata: Metadata = {
 export default function AdminPage() {
   return (
     <DashboardLayout type="admin">
-      <h1 className="text-slate-800 font-bold text-4xl mt-8">{metadata.title as string}</h1>
-      <h2 className="text-slate-700 text-xl pt-3 mb-8">{metadata.description as string}</h2>
+      <section className="flex items-center gap-4">
+        <CgLayoutList className="text-[4.5rem] text-danger-500" />
+        <div>
+          <h1 className="text-slate-800 font-bold text-4xl mt-8">{metadata.title as string}</h1>
+          <h2 className="text-slate-700 text-xl pt-3 mb-8">{metadata.description as string}</h2>
+        </div>
+      </section>
       <CategoryClient />
     </DashboardLayout>
   );
