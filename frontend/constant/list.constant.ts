35c89f4f8056dbcb461b57035b505c46d78e6679
Xnaizer <seykig028@gmail.com>
2026-01-19 14:59:46 +0700

    chore: update limit options and endpoints for category service

--- frontend/constant/list.constant.ts
+++ frontend/constant/list.constant.ts
@@ -1,13 +1,16 @@
 const LIMIT_LISTS = [
     {
-        label: '8', value: 8
+        label: '5', value: 5
     },
     {
-        label: '12', value: 12
+        label: '8', value: 8
     },
     {
         label: '10', value: 10
     },
+    {
+        label: '12', value: 12
+    }
 ]
 
 export { LIMIT_LISTS }
