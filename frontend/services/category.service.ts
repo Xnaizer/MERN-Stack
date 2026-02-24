10ac3244de909aca88e94d80f261545327713d68
Xnaizer <seykig028@gmail.com>
2026-02-24 21:39:57 +0700

    (feat: cat ser): add delete categories instance

--- frontend/services/category.service.ts
+++ frontend/services/category.service.ts
@@ -4,7 +4,8 @@ import { ICreateCategory } from "@/types/Category";
 
 const categoryServices = {
     getCategories: (params: string) => instance.get(`${endpoint.CATEGORY}?${params}`),
-    createCategories: (payload: ICreateCategory) => instance.post(`${endpoint.CATEGORY}`, payload)
+    createCategories: (payload: ICreateCategory) => instance.post(`${endpoint.CATEGORY}`, payload),
+    deleteCategories: (params: string) => instance.delete(`${endpoint.CATEGORY}/${params}`)
 }
 
 export default categoryServices;
